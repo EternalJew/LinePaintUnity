44b3996630a66c492ea7126baf8167c8d736ab0d
Oleksiy <58302277+EternalJew@users.noreply.github.com>
2021-07-15 17:25:31 +0300

    MoveBrushOnGrid

--- LinePaint/Assets/LinePaint/Script/LevelManager.cs
+++ LinePaint/Assets/LinePaint/Script/LevelManager.cs
@@ -6,35 +6,62 @@ namespace Linepaint
 {
     public class LevelManager : MonoBehaviour
     {
-        [SerializeField] private GameObject blockPrefab;
+        [SerializeField] private CameraZoom gameCamera;
+        [SerializeField] private Cell blockPrefab;
+        [SerializeField] private BrushController brush;
         [SerializeField] private int width;
         [SerializeField] private int height;
         [SerializeField] private float cellSize;
+        private Cell[,] cellArray;
         private Grid grid;
         private SwipeController swipeController;
+        private BrushController currentBrush;
         private void Start()
         {
             swipeController = new SwipeController();
             swipeController.SetLevelManager(this);
             grid = new Grid();
             grid.Initialize(width, height, cellSize);
+            cellArray = new Cell[width, height];
 
-            CreateGrid();
+            CreateGrid(Vector3.zero);
+
+            currentBrush = Instantiate(brush, grid.GetCellWorldPosition(0, 0), Quaternion.identity);
+            currentBrush.currentCoords = new Vector2Int(0, 0);
+            
+            gameCamera.ZoomPerspectiveCamera(width, height);
         }
-        private void CreateGrid()
+        private void CreateGrid(Vector3 originPos)
         {
             for (int x = 0; x < grid.GridArray.GetLength(0); x++)
             {
                 for(int y = 0; y < grid.GridArray.GetLength(1); y++)
                 {
-                    GameObject block = Instantiate(blockPrefab);
-                    block.transform.position = grid.GetCellWorldPosition(x, y);
+                    cellArray[x,y] = CreateCell(x, y, originPos);
                 }
             }
         }
+        private Cell CreateCell(int x, int y, Vector3 originPos)
+        {
+            Cell cell = Instantiate(blockPrefab);
+            cell.cellCoords = new Vector2Int(x,y);
+            cell.transform.localScale = new Vector3(cellSize, 0.25f, cellSize);
+            cell.transform.position = originPos + grid.GetCellWorldPosition(x,y);
+            
+            return cell;
+        }
         public void MoveBrush(Swipe direction)
         {
-            Debug.Log(direction);
+            //Debug.Log(direction);
+            Vector2Int newCoords = grid.GetCellXZBySwipe(currentBrush.currentCoords.x, currentBrush.currentCoords.y, direction);
+
+            if(newCoords != new Vector2Int(-1, -1))
+            {
+                Vector3 finalPos = grid.GetCellWorldPosition(newCoords.x, newCoords.y);
+
+                currentBrush.transform.position = finalPos;
+                currentBrush.currentCoords = newCoords;
+            }
         }
         private void Update()
         {
